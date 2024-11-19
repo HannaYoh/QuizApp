62aae7b106ee96c82aed3900b54f104dee434cfc
Hanna Yohannes <hannayohmek@gmail.com>
2024-11-20 00:55:55 +0300

    Image and buttons added

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -5,6 +5,34 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Text('start screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.keyboard_arrow_right_sharp),
+            label: const Text(
+              'Start quiz',
+              // style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
