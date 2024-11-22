b9a293487693779e100d903ff813af396ca1c41a
Hanna Yohannes <hannayohmek@gmail.com>
2024-11-22 23:01:28 +0300

    Different implementation for page navigation

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -5,6 +5,7 @@ import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -12,18 +13,10 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
-    setState(() {
-      activeScreen = const QuestionsScreen();
-    });
+    activeScreen = 'questions-screen';
   }
 
   @override
@@ -41,7 +34,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
